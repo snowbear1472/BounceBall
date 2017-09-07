390b6de0d8a3f2ff97e99f20fff64faf74caeb87
snowbear1472 <snowbear1472@gmail.com>
2017-09-07 22:08:37 +0900

    UI관련 항목 추가

--- BounceBall/include/UI/StackMenu.hh
+++ BounceBall/include/UI/StackMenu.hh
@@ -1,1 +1,43 @@
 #pragma once
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <memory>
+
+#include "Widget.hh"
+
+
+namespace BounceBall
+{
+	namespace UI
+	{
+		class StackMenu
+		{
+		public:
+			StackMenu( const sf::RenderWindow& handle );
+			StackMenu( const sf::Vector2f& pos );
+			StackMenu( StackMenu&& other );
+
+			~StackMenu( ) = default;
+
+		public:
+			StackMenu& operator=( StackMenu&& other );
+
+		public:
+			void add_widget( std::unique_ptr<Widget> );
+
+			void handle_event( sf::Event e, const sf::RenderWindow& handle );
+			void render( sf::RenderTarget& handle );
+
+		private:
+			void init_widget( Widget& widget );
+
+		private:
+			std::vector<std::unique_ptr<Widget>> widgets_;
+			sf::RectangleShape background_;
+
+			sf::Vector2f base_pos_;
+			sf::Vector2f base_size_;
+		};
+	}
+}
