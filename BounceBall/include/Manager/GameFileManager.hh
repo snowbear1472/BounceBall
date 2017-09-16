9fbc5220b286bb806b4f17f2cca8c07577b2acea
snowbear <snowbear1472@gmail.com>
2017-09-17 02:00:20 +0900

    GameFileManager 초기화

--- BounceBall/include/Manager/GameFileManager.hh
+++ BounceBall/include/Manager/GameFileManager.hh
@@ -12,30 +12,7 @@ namespace BounceBall
 		class GameFileManager final
 		{
 		public:
-			enum class Type
-			{
-				entity,
-				object
-			};
 
-		public:
-			static GameFileManager& get( );
-
-		private:
-			GameFileManager( );
-
-		public:
-			csv_map get( const std::string& name, const Type& type );
-			Entity* get_entity( const std::string& name );
-			Object* get_object( const std::string& name );
-
-		private:
-			path get_fullpath( const std::string& name, const Type& type );
-
-		public:
-			std::string dir_entity_;
-			std::string dir_object_;
-			std::string extension_;
 		};
 	}
 }
